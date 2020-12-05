98fa18e9c8f79cb20e7b7c4b773cd60d29a3ec1d
hyi <hongyi@renci.org>
2020-12-04 21:22:31 -0500

    further improved query performance and added a convenience management command

---
+++ server/rs_core/management/commands/remove_user_annotation.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from rs_core.models import UserImageAnnotation
+
+
+class Command(BaseCommand):
+    """
+    This script removes user annotation
+    To run this command, do:
+    docker exec -ti dot-server python manage.py remove_user_annotation <username>
+    For example:
+    docker exec -ti dot-server python manage.py remove_user_annotation <username>
+    """
+    help = "Remove user annotation from a user specified by input parameter <username>"
+
+    def add_arguments(self, parser):
+        # csv filename with full path to load metadata from
+        parser.add_argument('user_name', help='user name to remove user annotation for')
+
+    def handle(self, *args, **options):
+        user_name = options['user_name']
+        UserImageAnnotation.objects.filter(user__username=user_name).delete()
+        print('Done')
