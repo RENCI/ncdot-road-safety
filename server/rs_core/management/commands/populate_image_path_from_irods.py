71544ac6b759e8bde9cb912eae48a696bd04cca6
hyi <hongyi@renci.org>
2021-01-07 22:20:19 -0500

    handled exception in populate image path management command

--- server/rs_core/management/commands/populate_image_path_from_irods.py
+++ server/rs_core/management/commands/populate_image_path_from_irods.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django_irods.storage import IrodsStorage
-
+from django.core.exceptions import ValidationError
 from rs_core.models import RouteImage
 
 
@@ -18,12 +18,17 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         istorage = IrodsStorage()
         irods_prefix_str = '/ncdotZone/home/dotProxyUser/images/'
-        for obj in RouteImage.objects.all():
-            path = istorage.get_image_coll_path(f'{obj.image_base_name}5.jpg')
-            if path.startswith(irods_prefix_str):
-                path = path[len(irods_prefix_str):]
-                obj.image_path = path
-                obj.save()
-            else:
-                print(f'{path} does not start with irods_prefix_str', flush=True)
+        for obj in RouteImage.objects.filter(image_path__exact=''):
+            try:
+                path = istorage.get_image_coll_path(f'{obj.image_base_name}5.jpg')
+                if path.startswith(irods_prefix_str):
+                    path = path[len(irods_prefix_str):]
+                    obj.image_path = path
+                    obj.save()
+                else:
+                    print(f'{path} does not start with irods_prefix_str', flush=True)
+            except ValidationError as ex:
+                # image cannot be found, delete it
+                print(str(ex), flush=True)
+                obj.delete()
         print('Done')
