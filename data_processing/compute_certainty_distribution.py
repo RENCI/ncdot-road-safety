536a82a65e607a8276278ed3ef3d4895c297ec12
hyi <hongyi@renci.org>
2021-01-09 14:53:28 -0500

    refined certainty distribution computation a bit

--- data_processing/compute_certainty_distribution.py
+++ data_processing/compute_certainty_distribution.py
@@ -10,11 +10,15 @@ if __name__ == '__main__':
 
     input_file = sys.argv[1]
     df = pd.read_csv(input_file, header=0, index_col='MAPPED_IMAGE', dtype={'MAPPED_IMAGE': 'str',
-                                                                            'Probability': 'float'})
+                                                                            'ROUND_PREDICT': 'float'})
     df.dropna(inplace=True)
-    prob_series = df['Probability']
+    prob_series = df['ROUND_PREDICT']
     plt.hist(prob_series, bins=10, log=True)
     plt.show()
     print("mean:", prob_series.mean())
     print("std:", prob_series.std())
+    prob_series_yes = df['ROUND_PREDICT'][df['ROUND_PREDICT'] >= 0.5]
+    prob_series_no = df['ROUND_PREDICT'][df['ROUND_PREDICT'] < 0.5]
+    print(f'{len(prob_series_yes)}, {len(prob_series_no)}, {len(prob_series)}')
+
 
