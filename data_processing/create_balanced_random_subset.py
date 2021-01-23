0b21ea91ccf287461a90198e235e3d1308583773
hyi <hongyi@renci.org>
2021-01-23 13:36:57 -0500

    added a new data processing script for streamlining balanced random subset creation

---
+++ data_processing/create_balanced_random_subset.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import argparse
+
+from create_balanced_random_subset_html_for_inspection import create_html_file_for_inspection
+
+
+parser = argparse.ArgumentParser(description='Process arguments.')
+parser.add_argument('--input_file', type=str,
+                    default='../server/metadata/model-related/secondary_road/model_2lane_predict_d13.csv',
+                    help='input model prediction file with path for mapped images to create random subset from')
+parser.add_argument('--subset_size', type=int, default=100, help='number of images in the subset')
+parser.add_argument('--division', type=str, default='d13', help='division str on the path')
+parser.add_argument('--output_irods_file', type=str,
+                    default='../server/metadata/irods/images_to_be_registered_d13.csv',
+                    help='output file for the images to be registered in iRODS')
+parser.add_argument('--output_html_file', type=str, default='../server/templates/model_2lane_d13_subset.html',
+                    help='the output html file for looking at images')
+
+args = parser.parse_args()
+input_file = args.input_file
+subset_size = args.subset_size
+division = args.division
+output_irods_file = args.output_irods_file
+output_html_file = args.output_html_file
+
+
+df = pd.read_csv(input_file, header=0, index_col=False, dtype={'MAPPED_IMAGE': 'str', 'ROUND_PREDICT': 'float'},
+                 usecols=['MAPPED_IMAGE', 'ROUND_PREDICT'])
+print(df.shape)
+df['PATH'] = df['MAPPED_IMAGE'].str.slice(stop=-15)
+df['MAPPED_IMAGE'] = df['MAPPED_IMAGE'].str.replace('.jpg', '')
+df['MAPPED_IMAGE'] = df['MAPPED_IMAGE'].str.split('/').str[-1]
+
+df_yes = df[df.ROUND_PREDICT >= 0.5]
+df_no = df[df.ROUND_PREDICT < 0.5]
+print(df_yes.shape, df_no.shape)
+sample_size = (int)(subset_size / 2)
+sub_df = pd.concat([df_yes.sample(n=sample_size, random_state=1),
+                    df_no.sample(n=sample_size, random_state=1)])
+print(sub_df.shape)
+
+create_html_file_for_inspection(sub_df, output_html_file, two_models=False)
+
+prefix_str = f'/projects/ncdot/NC_2018_Secondary/images/{division}/'
+irods_df = prefix_str + sub_df['PATH'] + sub_df['MAPPED_IMAGE'] + '.jpg'
+irods_df.to_csv(output_irods_file, index=False)
+print('Done')
--- data_processing/create_balanced_random_subset_html_for_inspection.py
+++ data_processing/create_balanced_random_subset_html_for_inspection.py
@@ -2,43 +2,59 @@ import argparse
 import pandas as pd
 
 
-parser = argparse.ArgumentParser(description='Process arguments.')
-parser.add_argument('--input_file', type=str, default='../server/metadata/balanced_d4_subset_for_manual_inspection.csv',
-                    help='input image file with model prediction')
-parser.add_argument('--output_html_file', type=str, default='../server/templates/model_2lane_d4_subset.html',
-                    help='the output html file for looking at images')
+def create_html_file_for_inspection(input_df, output_file_name, two_models = True):
+    html_str_head = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Show randomly selected 100 secondary road images used for assessing the model classifier</title>
+    </head>
+    <body>    
+    """
+    html_str_tail = """
+    </body>
+    </html>
+    """
 
-args = parser.parse_args()
-input_file = args.input_file
-output_html_file = args.output_html_file
+    img_str = """
+    <div style="display: flex;">
+        <img src='/get_image_by_name/{image_base_name}.jpg' style="float:left;width:100%;"/>    
+    </div>
+    <p>Image base name: {image_base_name}, 2 lane model prediction: {prob2}
+    """
+    if two_models:
+        img_str += ", 4 lane model prediction: {prob4}</p>"
+    else:
+        img_str += "</p>"
 
-df = pd.read_csv(input_file, header=0, dtype=str, usecols=['MAPPED_IMAGE', 'ROUND_PREDICT_2', 'ROUND_PREDICT_4'])
-print(df.shape)
-html_str_head = """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Show randomly selected 100 secondary road images used for assessing the model classifier</title>
-</head>
-<body>    
-"""
-html_str_tail = """
-</body>
-</html>
-"""
+    img_str_list = []
+    if two_models:
+        input_df.apply(lambda row: img_str_list.append(img_str.format(image_base_name=row['MAPPED_IMAGE'],
+                                                                      prob2=row['ROUND_PREDICT_2'],
+                                                                      prob4=row['ROUND_PREDICT_4'])), axis=1)
+    else:
+        input_df.apply(lambda row: img_str_list.append(img_str.format(image_base_name=row['MAPPED_IMAGE'],
+                                                                      prob2=row['ROUND_PREDICT'])), axis=1)
+    html_img_str = '\n'.join(img_str_list)
+    html_str = f"{html_str_head}\n{html_img_str}\n{html_str_tail}"
+    with open(output_file_name, 'w') as file:
+        file.write(html_str)
 
-img_str = """
-<div style="display: flex;">
-    <img src='/get_image_by_name/{image_base_name}.jpg' style="float:left;width:100%;"/>    
-</div>
-<p>Image base name: {image_base_name}, 2 lane model prediction: {prob2}, 4 lane model prediction: {prob4}</p>
-"""
-img_str_list = []
-df.apply(lambda row: img_str_list.append(img_str.format(image_base_name=row['MAPPED_IMAGE'],
-                                                        prob2=row['ROUND_PREDICT_2'],
-                                                        prob4=row['ROUND_PREDICT_4'])), axis=1)
-html_img_str = '\n'.join(img_str_list)
-html_str = f"{html_str_head}\n{html_img_str}\n{html_str_tail}"
-with open(output_html_file, 'w') as file:
-    file.write(html_str)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process arguments.')
+    parser.add_argument('--input_file', type=str,
+                        default='../server/metadata/balanced_d4_subset_for_manual_inspection.csv',
+                        help='input image file with model prediction')
+    parser.add_argument('--output_html_file', type=str, default='../server/templates/model_2lane_d4_subset.html',
+                        help='the output html file for looking at images')
+
+    args = parser.parse_args()
+    input_file = args.input_file
+    output_html_file = args.output_html_file
+
+    df = pd.read_csv(input_file, header=0, dtype=str, usecols=['MAPPED_IMAGE', 'ROUND_PREDICT_2', 'ROUND_PREDICT_4'])
+    print(df.shape)
+    create_html_file_for_inspection(df, output_html_file)
+    print('Done')
