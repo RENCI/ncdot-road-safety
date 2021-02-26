c6b3ca81fd392d30a1df0d5a8fabf8259db9d5a8
hyi <hongyi@renci.org>
2021-02-26 15:26:03 -0500

    refined prepare images for al script

--- machine-learning/prepare_images_for_active_learning.py
+++ machine-learning/prepare_images_for_active_learning.py
@@ -2,16 +2,16 @@ import pandas as pd
 import argparse
 import os
 
-from utils import split_to_train_valid_test, create_yes_no_sub_dirs
+from utils import split_to_train_valid_test_for_al, create_yes_no_sub_dirs
 
 
 parser = argparse.ArgumentParser(description='Process arguments.')
 parser.add_argument('--input_file', type=str,
-                    default='../server/metadata/user_annoted_image_info.txt',
+                    default='/projects/ncdot/NC_2018_Secondary/active_learning/guardrail/round1/annot_data/user_annots.txt',
                     help='input file with path for user annotated images to create image data for active learning')
 parser.add_argument('--cur_round', type=int, default=1,
                     help='the current round of active learning')
-parser.add_argument('--train_frac', type=float, default='0.6',
+parser.add_argument('--train_frac', type=float, default='0.8',
                     help='fraction of training data over all data')
 parser.add_argument('--feature_name', type=str, default='guardrail',
                     help='the name of the feature for the classifier, e.g., guardrail')
@@ -31,25 +31,26 @@ train_frac = args.train_frac
 input_prefix_dir = args.input_prefix_dir
 root_dir = args.root_dir
 root_al_dir = os.path.join(root_dir, feature_name, f'round{cur_round}', 'data')
-df = pd.read_csv(input_file, header=0, index_col=False, dtype=str)
+df = pd.read_csv(input_file, header=0, index_col=False, dtype=str, usecols=['Image', 'Presence'])
+df.drop_duplicates(subset=['Image'])
 print(df.shape)
 
-train_df, valid_df, test_df = split_to_train_valid_test(df, 'Presence', train_frac)
+train_df, valid_df, test_df = split_to_train_valid_test_for_al(df, 'Presence', train_frac)
 print('training data:', len(train_df), 'validation data:', len(valid_df), 'test data:', len(test_df))
 
 train_path = f'{root_al_dir}/train/'
 create_yes_no_sub_dirs(train_path)
-train_df.apply(lambda row: os.symlink('{}/{}.jpg'.format(input_prefix_dir, row['Image']),
-                                      '{}{}/{}'.format(train_path, 'yes' if row['Presence'] == 'True' else 'no',
-                                                       row['Image'])), axis=1)
+train_df.apply(lambda row: os.symlink(os.path.join(input_prefix_dir, row['Image']),
+                                      os.path.join(train_path, 'yes' if row['Presence'] == 'True' else 'no',
+                                                   row['Image'])), axis=1)
 valid_path = f'{root_al_dir}/validation/'
 create_yes_no_sub_dirs(valid_path)
-valid_df.apply(lambda row: os.symlink('{}/{}.jpg'.format(input_prefix_dir, row['Image']),
-                                      '{}{}/{}'.format(valid_path, 'yes' if row['Presence'] == 'True' else 'no',
-                                                       row['Image'])), axis=1)
+valid_df.apply(lambda row: os.symlink(os.path.join(input_prefix_dir, row['Image']),
+                                      os.path.join(valid_path, 'yes' if row['Presence'] == 'True' else 'no',
+                                                    row['Image'])), axis=1)
 test_path = f'{root_al_dir}/test/'
 create_yes_no_sub_dirs(test_path)
-test_df.apply(lambda row: os.symlink('{}/{}.jpg'.format(input_prefix_dir, row['Image']),
-                                     '{}{}/{}'.format(test_path, 'yes' if row['Presence'] == 'True' else 'no',
-                                                      row['Image'])), axis=1)
+test_df.apply(lambda row: os.symlink(os.path.join(input_prefix_dir, row['Image']),
+                                     os.path.join(test_path, 'yes' if row['Presence'] == 'True' else 'no',
+                                                  row['Image'])), axis=1)
 print('Done')
