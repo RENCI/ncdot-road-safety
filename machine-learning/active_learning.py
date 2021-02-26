8b1a3f1409cec4d8b1353d78e88a06b85f579a54
hyi <hongyi@renci.org>
2021-02-26 11:21:49 -0500

    fine tune active learning script

--- machine-learning/active_learning.py
+++ machine-learning/active_learning.py
@@ -52,8 +52,8 @@ def get_train_val_data(bat_size):
     return normalized_train_ds, len(train_ds), normalized_valid_ds, len(val_ds)
 
 
-def get_model():
-    feature_model = tf.keras.models.load_model(model_file)
+def get_model(input_file):
+    feature_model = tf.keras.models.load_model(input_file)
     # print(feature_model.summary())
     feature_model.trainable = False
     # only make the top dense classification layer (2049 parameters) trainable
@@ -80,7 +80,7 @@ def make_inference(feature_model, bat_size, threshold=0.5):
         # Specify the classes explicitly
         class_names=['no', 'yes'],
         image_size=(299, 299),
-        batch_size=batch_size)
+        batch_size=bat_size)
     normalized_test_ds = test_ds.map(lambda x, y: (normalization_layer(x), y))
     normalized_test_ds = normalized_test_ds.cache().prefetch(buffer_size=AUTOTUNE)
     ts = time.time()
@@ -88,6 +88,10 @@ def make_inference(feature_model, bat_size, threshold=0.5):
     te = time.time()
     print('time taken for model inference on test set:', te - ts)
     y_pred = [1 if y[0] >= threshold else 0 for y in predictions]
+    #labels = []
+    #for image_batch, labl_batch in test_ds:
+    #    for lbl in labl_batch:
+    #        labels.append(lbl.numpy()[0])
     labels = test_ds.labels
     print('Confusion Matrix')
     print(confusion_matrix(labels, y_pred))
@@ -105,7 +109,6 @@ if __name__ == '__main__':
                         help='the number of total epoch to train the model')
     parser.add_argument('--batch_size', type=int, default=128,
                         help='batch size for training the model')
-
     parser.add_argument('--model_file', type=str,
                         default='/projects/ncdot/2018/machine_learning/model/guardrail_xception_2lane_epoch_10.h5',
                         help='model file with path to be loaded as the base model for further training')
@@ -129,14 +132,14 @@ if __name__ == '__main__':
     make_inference_only = args.make_inference_only
 
     setup_gpu_memory()
+    strategy = tf.distribute.MirroredStrategy()
     if not make_inference_only:
         callbacks_list = get_call_backs_list()
 
         norm_train_ds, train_ds_len, norm_val_ds, val_ds_len = get_train_val_data(batch_size)
 
-        strategy = tf.distribute.MirroredStrategy()
         with strategy.scope():
-            model = get_model()
+            model = get_model(model_file)
 
         ts = time.time()
         history = model.fit(norm_train_ds, epochs=num_of_epoch, callbacks=callbacks_list,
@@ -148,6 +151,7 @@ if __name__ == '__main__':
         print(history.history)
         model.save(output_model_file)
     else:
-        model = tf.keras.models.load_model(model_file)
+        with strategy.scope():
+            model = tf.keras.models.load_model(model_file)
     make_inference(model, batch_size*2)
     print('Done')
