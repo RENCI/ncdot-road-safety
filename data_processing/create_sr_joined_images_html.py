eb5630a303e6dfed9a71818db1719e9014464187
hyi <hongyi@renci.org>
2021-03-05 17:08:28 -0500

    updated scripts for model assessment for AL

--- data_processing/create_sr_joined_images_html.py
+++ data_processing/create_sr_joined_images_html.py
@@ -3,24 +3,24 @@ import pandas as pd
 
 
 parser = argparse.ArgumentParser(description='Process arguments.')
-parser.add_argument('--input_file', type=str, default='/projects/ncdot/secondary_road/model_2lane_predict_d4_subset.csv',
+parser.add_argument('--input_file', type=str, default='../server/metadata/model_2lane_predict_d4_with_manual_labels.csv',
                     help='input image file with model prediction')
-parser.add_argument('--output_html_file', type=str, default='/projects/ncdot/secondary_road/model_2lane_d4_subset.html',
+parser.add_argument('--output_html_file', type=str, default='../server/templates/model_2lane_d4_subset.html',
                     help='the output html file for looking at images')
 
 args = parser.parse_args()
 input_file = args.input_file
 output_html_file = args.output_html_file
 
-df = pd.read_csv(input_file, header=0, dtype=str, usecols=['file', 'prediction', 'class'])
+df = pd.read_csv(input_file, header=0, dtype=str)
+df['index'] = df['index'].str.replace('.jpg', '')
 print(df.shape)
-df['file'] = df['file'].str.slice(start=len('images/'))
 html_str_head = """
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Show randomly selected 100 secondary road images used for assessing the model classifier</title>
+    <title>Show d4 test holdout images with baseline model prediction info</title>
 </head>
 <body>    
 """
@@ -30,15 +30,18 @@ html_str_tail = """
 """
 
 img_str = """
-<div style="display: flex;">
-    <img src='/get_image_by_name/{image_base_name}' style="float:left;width:100%;"/>    
+<div style='display: flex;'>
+    <img src='/get_image_by_name/{image_base_name}5.jpg' style="float:left;width:33.33%;"/>
+    <img src='/get_image_by_name/{image_base_name}1.jpg' style="float:left;width:33.33%;"/>
+    <img src='/get_image_by_name/{image_base_name}2.jpg' style="float:left;width:33.33%;"/>        
 </div>
-<p>Image base name: {image_base_name}, prediction: {pred_class}({prob})</p>
+<p>Image base name: {image_base_name}, annotation: {annotation}, prediction: {prob} {WRONG}</p>
 """
 img_str_list = []
-df.apply(lambda row: img_str_list.append(img_str.format(image_base_name=row['file'],
-                                                        pred_class=row['class'],
-                                                        prob=row['prediction'])), axis=1)
+df.apply(lambda row: img_str_list.append(img_str.format(image_base_name=row['index'],
+                                                        annotation=row['Presence'],
+                                                        prob=row['ROUND_PREDICT'],
+                                                        WRONG="<b>WRONG</b>" if row['WRONG'] == 'Red' else '')), axis=1)
 html_img_str = '\n'.join(img_str_list)
 html_str = f"{html_str_head}\n{html_img_str}\n{html_str_tail}"
 with open(output_html_file, 'w') as file:
--- machine-learning/compute_model_classification_report.py
+++ machine-learning/compute_model_classification_report.py
@@ -8,27 +8,35 @@ from sklearn.metrics import average_precision_score
 
 parser = argparse.ArgumentParser(description='Process arguments.')
 parser.add_argument('--label_file', type=str,
-                    default='../server/metadata/training_Image_guardrail_yn.csv',
+                    default='../server/metadata/user_annotated_balanced_image_info_d4.txt',
                     help='labelled guardrail data obtained from guardrail survey data')
 parser.add_argument('--predict_label_file', type=str,
-                    default='../server/metadata/guardrail_classification.csv',
+                    default='../server/metadata/model-related/secondary_road/model_2lane_predict_d4.csv',
                     help='predicted guardrail lable data')
 
 args = parser.parse_args()
 label_file = args.label_file
 predict_label_file = args.predict_label_file
 
-df_labels = pd.read_csv(label_file, header=0, index_col='MAPPED_IMAGE', usecols=['MAPPED_IMAGE', 'GUARDRAIL_YN'])
-df_labels.GUARDRAIL_YN = np.where(df_labels.GUARDRAIL_YN.eq('Y'), 1, 0)
-df_labels.sort_index(inplace=True)
-df_predict_labels = pd.read_csv(predict_label_file, header=0, index_col='MAPPED_IMAGE')
-df_predict_labels.dropna(inplace=True)
-df_predict_labels.sort_index(inplace=True)
-df_predict_labels['PREDICTION_YN'] = np.where(df_predict_labels['Probability']>=0.5, 1, 0)
-df_result = pd.concat([df_labels, df_predict_labels], axis=1, join='inner')
+df_in = pd.read_csv(label_file, header=0, index_col=False, dtype={'Image': str, 'Presence': str},
+                    usecols=['Image', 'Presence'])
+df_in['Image'] = df_in['Image'].str.split('/').str[-1]
+df_al = pd.read_csv(predict_label_file, header=0, index_col=False,
+                    dtype={'MAPPED_IMAGE': str, 'ROUND_PREDICT': float},
+                    usecols=['MAPPED_IMAGE', 'ROUND_PREDICT'])
+df_al['MAPPED_IMAGE'] = df_al['MAPPED_IMAGE'].str.split('/').str[-1]
+df_al = df_al[df_al.MAPPED_IMAGE.isin(df_in.Image)]
+df_in = df_in.set_index('Image')
+df_al = df_al.set_index('MAPPED_IMAGE')
+df = pd.concat([df_in, df_al], axis=1)
+df = df.reset_index()
+df['Presence'] = df.Presence.apply(lambda row: 0 if row == 'False' else 1)
+df['ROUND_PREDICT'] = df.ROUND_PREDICT.apply(lambda row: 0 if row < 0.5 else 1)
+print(df.shape)
+df.to_csv('model_2lane_predict_d4_with_manual_labels.csv', index=False)
 print('Confusion Matrix')
-print(confusion_matrix(df_result['GUARDRAIL_YN'], df_result['PREDICTION_YN']))
+print(confusion_matrix(df['Presence'], df['ROUND_PREDICT']))
 print('Classification Report')
-print(classification_report(df_result['GUARDRAIL_YN'], df_result['PREDICTION_YN']))
-print('average precision score', average_precision_score(df_result['GUARDRAIL_YN'], df_result['PREDICTION_YN']))
+print(classification_report(df['Presence'], df['ROUND_PREDICT']))
+print('average precision score', average_precision_score(df['Presence'], df['ROUND_PREDICT']))
 #draw_plots(df_result['GUARDRAIL_YN'], df_result['PREDICTION_YN'])
