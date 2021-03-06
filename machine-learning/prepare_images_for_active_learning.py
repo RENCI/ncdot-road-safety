c92d5ca9e09050916c8cf2190538c7318fadb602
hyi <hongyi@renci.org>
2021-03-06 14:52:20 -0500

    fixed a couple of issues with updated script

--- machine-learning/prepare_images_for_active_learning.py
+++ machine-learning/prepare_images_for_active_learning.py
@@ -17,7 +17,7 @@ parser.add_argument('--feature_name', type=str, default='guardrail',
                     help='the name of the feature for the classifier, e.g., guardrail')
 parser.add_argument('--input_prefix_dir', type=str,
                     default='/projects/ncdot/NC_2018_Secondary/images/',
-                    help='root directory to create active learning data in - make sure it ends with the slash')
+                    help='root directory to create active learning data in - make sure it ends with slash')
 parser.add_argument('--root_dir', type=str,
                     default='/projects/ncdot/NC_2018_Secondary/active_learning',
                     help='root directory to create active learning data in')
@@ -46,15 +46,19 @@ exist_train_percent = args.exist_train_percent
 root_al_dir = os.path.join(root_dir, feature_name, f'round{cur_round}', 'data')
 df = pd.read_csv(input_file, header=0, index_col=False, dtype=str, usecols=['Image', 'Presence'])
 df = df.drop_duplicates(subset=['Image'])
-df.Image = input_prefix_dir + df.Image
+df.Full_Path_Image = input_prefix_dir + df.Image
 print(df.shape)
 df_yes_cnt = len(df[df.Presence == 'True'])
 df_no_cnt = len(df[df.Presence == 'False'])
 
-exist_train_yes_df = pd.read_csv(exist_train_yes_file, header=None, index_col=False, dtype = str, names=['Image'])
+exist_train_yes_df = pd.read_csv(exist_train_yes_file, header=None, index_col=False, dtype = str,
+                                 names=['Full_Path_Image'])
 exist_train_yes_df['Presence'] = 'True'
-exist_train_no_df = pd.read_csv(exist_train_no_file, header=None, index_col=False, dtype = str, names=['Image'])
+exist_train_yes_df['Image'] = exist_train_yes_df.Full_Path_Image.str.split('/').str[-1]
+exist_train_no_df = pd.read_csv(exist_train_no_file, header=None, index_col=False, dtype = str,
+                                names=['Full_Path_Image'])
 exist_train_no_df['Presence'] = 'False'
+exist_train_no_df['Image'] = exist_train_no_df.Full_Path_Image.str.split('/').str[-1]
 # train data is balanced, so it suffices to get count from either class
 exist_train_cnt = len(exist_train_no_df)
 exist_train_cnt_to_add = (int)(exist_train_cnt * exist_train_percent)
@@ -67,6 +71,7 @@ df = df.set_index('Image')
 exist_train_yes_df_to_add = exist_train_yes_df_to_add.set_index('Image')
 exist_train_no_df_to_add = exist_train_no_df_to_add.set_index('Image')
 df_all = pd.concat([df, exist_train_yes_df_to_add, exist_train_no_df_to_add])
+df_all = df_all.reset_index()
 
 
 def prepare_image(src, dst):
@@ -81,12 +86,12 @@ print('training data:', len(train_df), 'validation data:', len(valid_df))
 
 train_path = f'{root_al_dir}/train/'
 create_yes_no_sub_dirs(train_path)
-train_df.apply(lambda row: prepare_image(row['Image'],
+train_df.apply(lambda row: prepare_image(row['Full_Path_Image'],
                                          os.path.join(train_path, 'yes' if row['Presence'] == 'True' else 'no',
                                                       row['Image'])), axis=1)
 valid_path = f'{root_al_dir}/validation/'
 create_yes_no_sub_dirs(valid_path)
-valid_df.apply(lambda row: prepare_image(row['Image'],
+valid_df.apply(lambda row: prepare_image(row['Full_Path_Image'],
                                          os.path.join(valid_path, 'yes' if row['Presence'] == 'True' else 'no',
                                                       row['Image'])), axis=1)
 print('Done')
