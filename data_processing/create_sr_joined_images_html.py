2b44931d3bc69eac132845e687507efc11cad5d5
hyi <hongyi@renci.org>
2021-03-08 18:45:34 -0500

    added/updated processing scripts

--- data_processing/create_sr_joined_images_html.py
+++ data_processing/create_sr_joined_images_html.py
@@ -12,7 +12,8 @@ args = parser.parse_args()
 input_file = args.input_file
 output_html_file = args.output_html_file
 
-df = pd.read_csv(input_file, header=0, dtype=str)
+df = pd.read_csv(input_file, header=0, dtype={'index': str, 'Presence': str, 'ROUND_PREDICT': float,
+                                              'ROUND_PREDICT2': float, 'WRONG': int, 'WRONG2': int})
 df['index'] = df['index'].str.replace('.jpg', '')
 print(df.shape)
 html_str_head = """
@@ -20,7 +21,7 @@ html_str_head = """
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Show d4 test holdout images with baseline model prediction info</title>
+    <title>Show d4 test holdout images with baseline model and AL round 1 prediction info</title>
 </head>
 <body>    
 """
@@ -35,13 +36,15 @@ img_str = """
     <img src='/get_image_by_name/{image_base_name}1.jpg' style="float:left;width:33.33%;"/>
     <img src='/get_image_by_name/{image_base_name}2.jpg' style="float:left;width:33.33%;"/>        
 </div>
-<p>Image base name: {image_base_name}, annotation: {annotation}, prediction: {prob} {WRONG}</p>
+<p>Image base name: {image_base_name}, annotation: {annotation}, base model prediction: {prob} {WRONG}, round1 model prediction: {prob2} {WRONG2}</p>
 """
 img_str_list = []
 df.apply(lambda row: img_str_list.append(img_str.format(image_base_name=row['index'],
                                                         annotation=row['Presence'],
                                                         prob=row['ROUND_PREDICT'],
-                                                        WRONG="<b>WRONG</b>" if row['WRONG'] == 'Red' else '')), axis=1)
+                                                        WRONG="<b>WRONG</b>" if row['WRONG']==1 else '',
+                                                        prob2=row['ROUND_PREDICT2'],
+                                                        WRONG2="<b>WRONG</b>" if row['WRONG2']==1 else '')), axis=1)
 html_img_str = '\n'.join(img_str_list)
 html_str = f"{html_str_head}\n{html_img_str}\n{html_str_tail}"
 with open(output_html_file, 'w') as file:
--- machine-learning/compute_model_classification_report.py
+++ machine-learning/compute_model_classification_report.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import argparse
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.metrics import average_precision_score
@@ -11,7 +10,7 @@ parser.add_argument('--label_file', type=str,
                     default='../server/metadata/user_annotated_balanced_image_info_d4.txt',
                     help='labelled guardrail data obtained from guardrail survey data')
 parser.add_argument('--predict_label_file', type=str,
-                    default='../server/metadata/model-related/secondary_road/model_2lane_predict_d4.csv',
+                    default='../server/metadata/model_predict_test_base.csv',
                     help='predicted guardrail lable data')
 
 args = parser.parse_args()
@@ -33,7 +32,6 @@ df = df.reset_index()
 df['Presence'] = df.Presence.apply(lambda row: 0 if row == 'False' else 1)
 df['ROUND_PREDICT'] = df.ROUND_PREDICT.apply(lambda row: 0 if row < 0.5 else 1)
 print(df.shape)
-df.to_csv('model_2lane_predict_d4_with_manual_labels.csv', index=False)
 print('Confusion Matrix')
 print(confusion_matrix(df['Presence'], df['ROUND_PREDICT']))
 print('Classification Report')
