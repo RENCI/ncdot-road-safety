e4d85da702be1edc439da5aacbb0e7e4bde1d1b6
hyi <hongyi@renci.org>
2021-04-07 10:05:36 -0400

    minor update to create similarity scores script

--- data_processing/create_similarity_scores.py
+++ data_processing/create_similarity_scores.py
@@ -59,11 +59,12 @@ whole_df['SIMILARITY_YES'] = whole_df.apply(lambda row: get_cosine_similarity(np
 whole_df['SIMILARITY_NO'] = whole_df.apply(lambda row: get_cosine_similarity(np.asarray(row['FEATURES']),
                                                                              centroid_no_vec),
                                            axis=1)
-whole_df.drop(columns=['FEATURES'], inplace=True)
-whole_df = whole_df.sort_values(by=['SIMILARITY_YES', 'SIMILARITY_NO'])
+whole_df = whole_df.drop(columns=['FEATURES'])
 whole_df.to_csv(output_file, index=False)
+whole_df = whole_df.sort_values(by=['SIMILARITY_YES', 'SIMILARITY_NO'])
+whole_df.to_csv(output_file + '.sorted', index=False)
 whole_size = len(whole_df)
 # uncertainty reflects sorting by SCORE
 whole_df["UNCERTAINTY"] = whole_df.apply(lambda row: whole_size - whole_df.index.get_loc(row.name), axis=1)
-whole_df.to_csv(output_file, index=False)
+whole_df.to_csv(output_file + '.uncertainty', index=False)
 print('Done')
