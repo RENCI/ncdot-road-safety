7ee8eba8389a60e0149436794b201424eef6ac83
hyi <hongyi@renci.org>
2021-05-17 21:35:59 -0400

    make yes/no more balanced based on single view

--- machine-learning/prepare_images_for_active_learning.py
+++ machine-learning/prepare_images_for_active_learning.py
@@ -86,8 +86,10 @@ df_no_cnt = len(df[df.Presence == 'False'])
 print(df.shape, df_yes_cnt, df_no_cnt)
 if no_exist_train:
     # under-sample common negative class to make a balanced training set
-    df_yes = df[df.Presence == 'True']
-    df_no = df[df.Presence == 'False'].sample(n=df_yes_cnt, random_state=42)
+    df_yes = df[(df.LeftView == 'p') | (df.LeftView == 'i') | (df.FrontView == 'p') |
+                (df.FrontView == 'i') | (df.RightView == 'p') | (df.RightView == 'i')]
+    df_no = df[(df.LeftView == 'a') & (df.FrontView == 'a') & (df.RightView == 'a')]
+    df_no = df_no.sample(n=len(df_yes), random_state=42)
     df = pd.concat([df_yes, df_no])
 
 train_df_user, valid_df_user = split_to_train_valid_for_al(df, 'Presence', train_frac)
