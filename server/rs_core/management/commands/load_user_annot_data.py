b6198d78c1239f65cf6e5cf8fb5af28dee0918ce
hyi <hongyi@renci.org>
2021-06-01 14:45:17 -0400

    updated loading user annot data management command

--- server/rs_core/management/commands/load_user_annot_data.py
+++ server/rs_core/management/commands/load_user_annot_data.py
@@ -7,10 +7,10 @@ class Command(BaseCommand):
     """
     This script load user annotated data from csv file into database
     To run this command, do:
-    docker exec -ti dot-server python manage.py load_user_annot_data <input_csv_file_with_path>
+    docker exec -ti dot-server python manage.py load_user_annot_data <input_csv_file_with_path> <annot_name>
     For example:
     docker exec -ti dot-server python manage.py load_user_annot_data
-    metadata/model-related/secondary_road/d4_subset_with_manual_inspection_300.csv hongyi guardrail
+    metadata/all_user_annots.txt guardrail
     """
     help = "Process the user annotation file in csv format to load into database"
 
@@ -18,16 +18,17 @@ class Command(BaseCommand):
         # csv filename with full path to load metadata from
         parser.add_argument('input_file', help='input csv file name with full path to be '
                                                'processed and load user annotation from')
-        parser.add_argument('user_name', help='username of the user whose annotation needs to be saved')
         parser.add_argument('annot_name', help='annotation name for which annotation needs to be saved')
 
 
     def handle(self, *args, **options):
         input_file = options['input_file']
-        user_name = options['user_name']
         annot_name = options['annot_name']
-        df = pd.read_csv(input_file, header=0, index_col=False, dtype=str, usecols=["MAPPED_IMAGE",
-                                                                                    "MANUAL_YN"])
-        df.apply(lambda row: save_annot_data_to_db(row['MAPPED_IMAGE'], user_name, annot_name,
-                                                   True if row['MANUAL_YN'] == 'Y' else False), axis=1)
+        df = pd.read_csv(input_file, header=0, index_col=False, usecols=['Image', 'Username', 'LeftView',
+                                                                         'FrontView', 'RightView'])
+        df.apply(lambda row: save_annot_data_to_db(row['Image'], row['Username'], annot_name,
+                                                   {'left': row['LeftView'],
+                                                    'front': row['FrontView'],
+                                                    'right': row['RightView']}), axis=1)
+
         print('Done')
