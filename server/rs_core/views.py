696bae7d2396b425e584e934656253d251969eee
hyi <hongyi@renci.org>
2021-06-09 22:12:12 -0400

    updated to set presence as N/A if user annotations are not available

--- server/rs_core/views.py
+++ server/rs_core/views.py
@@ -378,10 +378,11 @@ def get_route_info(request, route_id):
         if feature_name:
             image_dict['probability'] = image_dict.pop('aiimageannotation__certainty')
             image_dict['presence'] = image_dict.pop('userimageannotation__presence')
-            if not image_dict['presence']:
-                image_dict['presence'] = True if image_dict['probability'] >= 0.5 else False
+            if image_dict['presence'] is None:
+                image_dict['presence'] = 'N/A'
 
     return JsonResponse({'route_image_info': updated_route_images}, status=status.HTTP_200_OK)
+
 
 @login_required
 def get_annotation_set(request):
