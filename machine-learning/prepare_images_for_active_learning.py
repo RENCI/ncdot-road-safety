eb0dad7f61c4eb1735536fa0f7c17859d17e7008
hyi <hongyi@renci.org>
2021-06-18 17:41:38 -0400

    simplified prepare active learning image script and added option to treat irrelevant as negative for training

--- machine-learning/prepare_images_for_active_learning.py
+++ machine-learning/prepare_images_for_active_learning.py
@@ -37,26 +37,15 @@ parser.add_argument('--exist_train_no_file', type=str,
                     help='existing train negative data to select to add to the AL')
 parser.add_argument('--exist_train_percent', type=float, default=0.06,
                     help='existing train percentage to add to the AL round annotation data for the larger class count')
-parser.add_argument('--output_annot_train_file', type=str,
-                    default='/projects/ncdot/NC_2018_Secondary/active_learning/guardrail/round4/'
-                            'annot_data/all_user_annots_train.csv',
-                    help='annotated data used for AL training which is used for computing centroid of training data')
 parser.add_argument('--original_image_without_join', action='store_true', default=False,
                     help='if set, original image rather than joined images are prepared for AL')
-parser.add_argument('--include_all_neg_in_joined_pos', action='store_true', default=False,
-                    help='if set, negative single images are part of total negative images to randomly sample '
-                         'for a balanced train/validation set')
 parser.add_argument('--no_exist_train', action='store_true', default=False,
                     help='if set, no existing training data is added for AL')
-parser.add_argument('--neg_fps_file', type=str,
-                    default='/projects/ncdot/NC_2018_Secondary/active_learning/guardrail/round5/annot_data/'
-                            'annot_fps_from_round4_model.csv',
-                    help='the fps file that contaeins FPs images to be included in the negative samples')
-parser.add_argument('--neg_fence_percent', type=float, default=-1,
-                    help='percentage of fence in the negative set - only valid when it is between 0 and 1')
 parser.add_argument('--is_unbalanced', action='store_true', default=False,
                     help='If set, the data created do not have to be balanced but instead prepare all annotation '
                          'data without balancing; otherwise, undersample majority class to balance with minority class')
+parser.add_argument('--i_as_p', action='store_true', default=False,
+                    help='If set, irrlevant images will be treated as positive rather than negative by default')
 
 args = parser.parse_args()
 input_file = args.input_file
@@ -70,18 +59,15 @@ root_dir = args.root_dir
 exist_train_yes_file = args.exist_train_yes_file
 exist_train_no_file = args.exist_train_no_file
 exist_train_percent = args.exist_train_percent
-output_annot_train_file = args.output_annot_train_file
 original_image_without_join = args.original_image_without_join
-include_all_neg_in_joined_pos = args.include_all_neg_in_joined_pos
 no_exist_train = args.no_exist_train
-neg_fence_percent = args.neg_fence_percent
-neg_fps_file = args.neg_fps_file
 is_unbalanced = args.is_unbalanced
+i_as_p = args.i_as_p
 
 
 def read_annotation_df(df_file):
     annot_df = pd.read_csv(df_file, header=0, index_col=False, dtype=str,
-                           usecols=['Image', 'Presence', 'LeftView', 'FrontView', 'RightView', 'Flags'])
+                           usecols=['Image', 'Presence', 'LeftView', 'FrontView', 'RightView'])
     annot_df = annot_df.drop_duplicates(subset=['Image'])
     annot_df['Full_Path_Image'] = input_prefix_dir + annot_df.Image
     annot_df = annot_df.set_index('Image')
@@ -102,67 +88,56 @@ root_al_dir = os.path.join(root_dir, feature_name, f'round{cur_round}', 'data')
 if no_exist_train:
     # under-sample majority class to make a balanced training set
     if original_image_without_join:
-        df_yes = df[(df.LeftView == 'p') | (df.LeftView == 'i') | (df.FrontView == 'p') |
-                    (df.FrontView == 'i') | (df.RightView == 'p') | (df.RightView == 'i')]
-        df_yes.Presence = 'True'
-        df_yes_single_yes_cnt = len(df_yes[df_yes.LeftView == 'p']) + len(df_yes[df_yes.FrontView == 'p']) + \
-            len(df_yes[df_yes.RightView == 'p']) + len(df_yes[df_yes.LeftView == 'i']) + \
-            len(df_yes[df_yes.FrontView == 'i']) + len(df_yes[df_yes.RightView == 'i'])
-        df_yes_single_no_cnt = len(df_yes[df_yes.LeftView == 'a']) + len(df_yes[df_yes.FrontView == 'a']) + \
-            len(df_yes[df_yes.RightView == 'a'])
-        if include_all_neg_in_joined_pos:
-            if df_yes_single_yes_cnt > df_yes_single_no_cnt:
-                df_yes_single_diff_cnt = df_yes_single_yes_cnt - df_yes_single_no_cnt
-
-                df_no = df[(df.LeftView == 'a') & (df.FrontView == 'a') & (df.RightView == 'a')]
-                df_no_joined_cnt = df_yes_single_diff_cnt // 3 + 1
-                if neg_fps_file:
-                    neg_fps = pd.read_csv(neg_fps_file, index_col=False, dtype=str)
-                    df_no['MAPPED_IMAGE'] = df_no.index.str.split('/').str[-1].str.split('.').str[0]
-                    df_no_fps = df_no[df_no.MAPPED_IMAGE.isin(neg_fps.MAPPED_IMAGE)]
-                    df_no_other = df_no[~df_no.MAPPED_IMAGE.isin(neg_fps.MAPPED_IMAGE)]
-                    if not is_unbalanced:
-                        df_no_other = df_no_other.sample(n=df_no_joined_cnt-len(df_no_fps), random_state=42)
-                    df_no = pd.concat([df_no_fps, df_no_other])
-                    print('df_no_fps: ', len(df_no_fps), ', df_no_other:', len(df_no_other), 'df_no:', len(df_no))
-                elif 0 < neg_fence_percent < 1:
-                    df_no_fence = df_no[df_no.Flags == 'Fence']
-                    df_no_fence_cnt = len(df_no_fence)
-                    fence_cnt = int(df_no_joined_cnt * neg_fence_percent)
-                    if df_no_fence_cnt < fence_cnt:
-                        fence_cnt = df_no_fence_cnt
-                    elif not is_unbalanced:
-                        df_no_fence = df_no_fence.sample(n=fence_cnt, random_state=42)
-                    if not is_unbalanced:
-                        df_no_other = df_no[df_no.Flags != 'Fence'].sample(n=df_no_joined_cnt-fence_cnt,
-                                                                           random_state=42)
-                    df_no = pd.concat([df_no_fence, df_no_other])
-                elif not is_unbalanced:
-                    df_no = df_no.sample(n=df_no_joined_cnt, random_state=42)
-                df_no.Presence = 'False'
-                df = pd.concat([df_yes, df_no])
-            else:
-                df = df_yes
+        if i_as_p:
+            df_yes = df[(df.LeftView == 'p') | (df.LeftView == 'i') | (df.FrontView == 'p') |
+                        (df.FrontView == 'i') | (df.RightView == 'p') | (df.RightView == 'i')]
+            df_yes_single_yes_cnt = len(df_yes[df_yes.LeftView == 'p']) + len(df_yes[df_yes.FrontView == 'p']) + \
+                                    len(df_yes[df_yes.RightView == 'p']) + len(df_yes[df_yes.LeftView == 'i']) + \
+                                    len(df_yes[df_yes.FrontView == 'i']) + len(df_yes[df_yes.RightView == 'i'])
+            df_yes_single_no_cnt = len(df_yes[df_yes.LeftView == 'a']) + len(df_yes[df_yes.FrontView == 'a']) + \
+                                   len(df_yes[df_yes.RightView == 'a'])
+            df_yes.Presence = 'True'
         else:
-            # put negative images from the positive joined images into the total negative image sample pool
+            df_yes = df[(df.LeftView == 'p') | (df.FrontView == 'p') | (df.RightView == 'p')]
+            df_yes_single_yes_cnt = len(df_yes[df_yes.LeftView == 'p']) + len(df_yes[df_yes.FrontView == 'p']) + \
+                                    len(df_yes[df_yes.RightView == 'p'])
+            df_yes_single_no_cnt = len(df_yes[df_yes.LeftView == 'a']) + len(df_yes[df_yes.FrontView == 'a']) + \
+                                   len(df_yes[df_yes.RightView == 'a']) + len(df_yes[df_yes.LeftView == 'i']) + \
+                                    len(df_yes[df_yes.FrontView == 'i']) + len(df_yes[df_yes.RightView == 'i'])
+            print('df_yes_single_yes_cnt: ', df_yes_single_yes_cnt, ', df_yes_single_no_cnt: ', df_yes_single_no_cnt)
+
+        # put negative images from the positive joined images into the total negative image sample pool
+        if i_as_p:
             df_yes_no = df_yes[(df_yes.LeftView == 'a') | (df_yes.FrontView == 'a') | (df_yes.RightView == 'a')]
-            df_yes_no.Presence = 'False'
             df_no = df[(df.LeftView == 'a') & (df.FrontView == 'a') & (df.RightView == 'a')]
-            df_no.Presence = 'False'
-            if not is_unbalanced:
-                # half sample from df_yes_no and the other half sample from df_no
-                sample_cnt = df_yes_single_yes_cnt // 2
-                no_ratio = df_yes_single_no_cnt / (df_yes_single_yes_cnt + df_yes_single_no_cnt)
-                df_yes_no = df_yes_no.sample(n=int(sample_cnt * no_ratio) + 1, random_state=42)
-                df_no = df_no.sample(n=sample_cnt // 3 + 1, random_state=42)
-            df_no = pd.concat([df_yes_no, df_no])
-            # have to reset index since negative single images from the joined positive images could have the same
-            # image names as the positive single images, which would be lost without reset_index call
-            df.reset_index(inplace=True)
-            df = pd.concat([df_yes, df_no])
+        else:
+            df_yes_no = df_yes[(df_yes.LeftView == 'a') | (df_yes.FrontView == 'a') | (df_yes.RightView == 'a') |
+                               (df_yes.LeftView == 'i') | (df_yes.FrontView == 'i') | (df_yes.RightView == 'i')]
+            df_no = df[((df.LeftView == 'a') | (df.LeftView == 'i')) &
+                       ((df.FrontView == 'a') | (df.FrontView == 'i')) &
+                       ((df.RightView == 'a') | (df.RightView == 'i'))]
+
+        df_yes_no.Presence = 'False'
+
+        if not is_unbalanced:
+            # half sample from df_yes_no and the other half sample from df_no
+            sample_cnt = df_yes_single_yes_cnt // 2
+            no_ratio = df_yes_single_no_cnt / (df_yes_single_yes_cnt + df_yes_single_no_cnt)
+            df_yes_no = df_yes_no.sample(n=int(sample_cnt * no_ratio) + 1, random_state=42)
+            df_yes_no_single_yes_cnt = len(df_yes_no[df_yes_no.LeftView == 'p']) + \
+                                       len(df_yes_no[df_yes_no.FrontView == 'p']) + \
+                                       len(df_yes_no[df_yes_no.RightView == 'p'])
+            df_yes_no_single_no_cnt = len(df_yes_no) * 3 - df_yes_no_single_yes_cnt
+            sample_cnt = df_yes_single_yes_cnt - df_yes_no_single_no_cnt
+            df_no = df_no.sample(n=sample_cnt // 3 + 1, random_state=42)
+        df_no = pd.concat([df_yes_no, df_no])
+        # have to reset index since negative single images from the joined positive images could have the same
+        # image names as the positive single images, which would be lost without reset_index call
+        df.reset_index(inplace=True)
+        df = pd.concat([df_yes, df_no])
+        print('df.shape after concatenation: ', df.shape)
     elif not is_unbalanced:
         df_yes = df[df.Presence == 'True']
-        df_no = df[(df.Presence == 'False') & (df.LeftView != 'i') & (df.FrontView != 'i') & (df.RightView != 'i')]
         df_no = df[df.Presence == 'False']
         df_yes_cnt = len(df_yes)
         df_no = df_no.sample(n=df_yes_cnt, random_state=42)
@@ -173,7 +148,6 @@ if no_exist_train:
     valid_df = valid_df_user
 else:
     train_df_user, valid_df_user = split_to_train_valid_for_al(df, 'Presence', train_frac)
-    train_df_user.drop(columns=['Full_Path_Image']).to_csv(output_annot_train_file)
 
 if not no_exist_train:
     df_yes_cnt = len(df[df.Presence == 'True'])
@@ -233,12 +207,12 @@ train_df.apply(lambda row: prepare_image(row['Full_Path_Image'],
                                          os.path.join(train_path, 'yes' if row['Presence'] == 'True' else 'no',
                                                       row['Image']),
                                          row['LeftView'], row['FrontView'], row['RightView'], row['Presence'],
-                                         prepare_opposite=True if include_all_neg_in_joined_pos else False), axis=1)
+                                         prepare_opposite=False), axis=1)
 valid_path = f'{root_al_dir}/validation/'
 create_yes_no_sub_dirs(valid_path)
 valid_df.apply(lambda row: prepare_image(row['Full_Path_Image'],
                                          os.path.join(valid_path, 'yes' if row['Presence'] == 'True' else 'no',
                                                       row['Image']),
                                          row['LeftView'], row['FrontView'], row['RightView'], row['Presence'],
-                                         prepare_opposite=True if include_all_neg_in_joined_pos else False), axis=1)
+                                         prepare_opposite=False), axis=1)
 print('Done')
