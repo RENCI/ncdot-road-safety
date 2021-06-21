4fd66a255389fa9d0531086b1488554c3816958d
hyi <hongyi@renci.org>
2021-06-21 12:43:40 -0400

    added creating analysis plots by division

--- data_processing/compute_certainty_distribution.py
+++ data_processing/compute_certainty_distribution.py
@@ -6,17 +6,20 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process arguments.')
     parser.add_argument('--input_file', type=str,
-                        default='../server/metadata/model_predict_test_round3.csv',
+                        default='../server/metadata/pole/round3/predict_d08.csv',
                         help='input file with path with model predictions')
+    parser.add_argument('--title', type=str, default='D8 Prediction Distribution', help='title to put on the plot')
 
     args = parser.parse_args()
     input_file = args.input_file
+    title = args.title
 
     df = pd.read_csv(input_file, header=0, index_col='MAPPED_IMAGE', dtype={'MAPPED_IMAGE': 'str',
                                                                             'ROUND_PREDICT': 'float'})
     df.dropna(inplace=True)
     prob_series = df['ROUND_PREDICT']
     # prob_series = df[df.index.str.startswith('d13') | df.index.str.startswith('d14')]['ROUND_PREDICT']
+    plt.title(title)
     plt.hist(prob_series, bins=10, log=True)
     plt.show()
     print("mean:", prob_series.mean())
@@ -26,5 +29,3 @@ if __name__ == '__main__':
     prob_series_1 = df['ROUND_PREDICT'][df['ROUND_PREDICT'] == 1.0]
     print(f'{len(prob_series_yes)}, {len(prob_series_no)}, {len(prob_series)}')
     print(len(prob_series_1), len(prob_series_1)/len(prob_series))
-
-
--- machine-learning/create_prec_recall_threshold_plot.py
+++ machine-learning/create_prec_recall_threshold_plot.py
@@ -5,6 +5,21 @@ from sklearn.metrics import precision_recall_curve
 from utils import create_single_data_frame
 
 
+def filter_df_by_division(in_df, div_str, col_str):
+    """
+    filter dataframe df by division
+    :param in_df: input dataframe
+    :param div_str: division string to filter, d04, d08, d13, d14, or d1314 in which case both d13 and d14 are
+    used as filters
+    :param col_str: column string in the df to filter by division
+    :return: filtered df
+    """
+    if div_str == 'd1314':
+        return in_df[in_df[col_str].str.startswith('d13') | in_df[col_str].str.startswith('d14')]
+    else:
+        return in_df[in_df[col_str].str.startswith(division)]
+
+
 parser = argparse.ArgumentParser(description='Process arguments.')
 parser.add_argument('--input_file', type=str,
                     #default='../server/metadata/holdout_test/holdout_test_user_annots.csv',
@@ -18,13 +33,20 @@ parser.add_argument('--model_predict_file', type=str,
                     help='the active learning model prediction file')
 parser.add_argument('--single_image', type=bool, default=True,
                     help='if True, create prec_recall_threshold curve for single imagge rather than for joined image')
+parser.add_argument('--division', type=str, default='d08', help='if not None, create plot for the division(s)')
+parser.add_argument('--title', type=str, default='D8 precision recall threshold plot',
+                    help='title for the plot')
 
 args = parser.parse_args()
 input_file = args.input_file
 model_predict_file = args.model_predict_file
 single_image = args.single_image
+division = args.division
+title = args.title
 
 df_in = pd.read_csv(input_file, header=0, index_col=False, dtype=str)
+if division:
+    df_in = filter_df_by_division(df_in, division, 'Image')
 df_in['Image'] = df_in['Image'].str.split('/').str[-1]
 df_in['Image'] = df_in['Image'].str.replace('.jpg', '')
 df_in.set_index('Image', inplace=True)
@@ -35,6 +57,8 @@ if single_image:
 df_al = pd.read_csv(model_predict_file, header=0, index_col=False,
                     dtype={'MAPPED_IMAGE': str, 'ROUND_PREDICT': float},
                     usecols=['MAPPED_IMAGE', 'ROUND_PREDICT'])
+if division:
+    df_al = filter_df_by_division(df_al, division, 'MAPPED_IMAGE')
 df_al['MAPPED_IMAGE'] = df_al['MAPPED_IMAGE'].str.split('/').str[-1]
 df_al['MAPPED_IMAGE'] = df_al['MAPPED_IMAGE'].str.replace('.jpg', '')
 df_al = df_al[df_al.MAPPED_IMAGE.isin(df_in.index)]
@@ -44,7 +68,9 @@ df = df.reset_index()
 df['Presence'] = df.Presence.apply(lambda row: 0 if row == 'False' else 1)
 print(df.shape)
 
-precision, recall, thresholds = precision_recall_curve(df['Presence'].to_numpy(), df['ROUND_PREDICT'].to_numpy(), pos_label=1)
+precision, recall, thresholds = precision_recall_curve(df['Presence'].to_numpy(), df['ROUND_PREDICT'].to_numpy(),
+                                                       pos_label=1)
+plt.title(title)
 plt.plot(thresholds, precision[:-1], 'b--', linewidth=2, label='Precision')
 plt.plot(thresholds, recall[:-1], 'g-', linewidth=2, label='Recall')
 plt.xlabel('Threshold')
