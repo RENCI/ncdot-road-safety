6409ea7dacd0597384cbb989fd377e9c9102de91
hyi <hongyi@renci.org>
2021-07-05 17:20:20 -0400

    updated prepare final deliverable script to take prediction for missing images into consideration

--- data_processing/prepare_final_deliverable.py
+++ data_processing/prepare_final_deliverable.py
@@ -5,30 +5,38 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process arguments.')
     parser.add_argument('--input_map_file', type=str,
-                        default='/projects/ncdot/secondary_road/output/d13/mapped_2lane_sr_images_d13.csv',
+                        default='/projects/ncdot/secondary_road/output/d01/mapped_2lane_sr_images_d1.csv',
                         help='input mapping file with route id and mile post')
     parser.add_argument('--input_predict_file', type=str,
-                        default='/projects/ncdot/secondary_road/predict/guardrail/predict_d13_single.csv',
+                        default='/projects/ncdot/secondary_road/predict/guardrail/predict_d01_single.csv',
                         help='input model prediction file')
+    parser.add_argument('--input_predict_file_2', type=str,
+                        default='/projects/ncdot/secondary_road/predict/guardrail/predict_d01_single_missing.csv',
+                        help='input model prediction file for missing images to be added into input_predict_file')
     parser.add_argument('--threshold', type=float, default=0.8,
                         help='threshold for model binary classification')
     parser.add_argument('--output_file', type=str,
                         default='/projects/ncdot/secondary_road/predict/guardrail/deliverable/'
-                                'guardrail_model_predict_d13.csv',
+                                'guardrail_model_predict_d1.csv',
                         help='output file for final deliverable')
 
     args = parser.parse_args()
     input_map_file = args.input_map_file
     input_predict_file = args.input_predict_file
+    input_predict_file_2 = args.input_predict_file_2
     threshold = args.threshold
     output_file = args.output_file
 
     map_df = pd.read_csv(input_map_file, header=0, index_col=None, dtype=str,
                          usecols=['ROUTEID', 'MAPPED_IMAGE', 'MILE_POST'])
-    map_df.set_index('MAPPED_IMAGE', inplace=True)
     predict_df = pd.read_csv(input_predict_file, header=0, index_col=None,
                              dtype={'MAPPED_IMAGE': str,
                                     'ROUND_PREDICT': float})
+    if input_predict_file_2:
+        predict_df_2 = pd.read_csv(input_predict_file_2, header=0, index_col=None,
+                                   dtype={'MAPPED_IMAGE': str,
+                                          'ROUND_PREDICT': float})
+        predict_df = pd.concat([predict_df, predict_df_2])
     predict_df.MAPPED_IMAGE = predict_df.MAPPED_IMAGE.str.split('/').str[-1]
     predict_df.MAPPED_IMAGE = predict_df.MAPPED_IMAGE.str.replace('.jpg', '')
     predict_df['MAPPED_IMAGE'] = predict_df.MAPPED_IMAGE.str[:-1]
@@ -36,9 +44,12 @@ if __name__ == '__main__':
     map_size = len(map_df)
     predict_size = len(predict_df)
     if map_size != predict_size:
-        print(f'mapping file and prediction file have different size, exiting. {map_size} != {predict_size}')
-        exit(1)
+        # use predict_size to filter map data frame to accommodate some malformed mapped images that cannot be predicted
+        map_df = map_df[map_df.MAPPED_IMAGE.isin(predict_df.index)]
+        print(f'after filtering map_df by predict_df, map_df size is {len(map_df)}, predict_df size is {predict_size}')
+
     predict_df['PRESENCE'] = predict_df.apply(lambda row: True if row.ROUND_PREDICT >= threshold else False, axis=1)
+    map_df.set_index('MAPPED_IMAGE', inplace=True)
     df = pd.concat([map_df, predict_df], axis=1)
     df.sort_values(by=['ROUTEID', 'MILE_POST'], inplace=True)
     df.reset_index(inplace=True)
