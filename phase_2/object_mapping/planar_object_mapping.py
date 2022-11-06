edaa2d0067a6f11e1a2d8de84cc24799b20cce18
hyi <hongyi@renci.org>
2022-11-06 17:27:07 -0500

    better comments with mapping code and added planar object mapping code

--- phase_2/object_mapping/planar_object_mapping.py
+++ phase_2/object_mapping/planar_object_mapping.py
@@ -4,31 +4,20 @@ import os
 import os.path
 import time
 import numpy as np
-from math import radians, pi, cos, sin, asin, sqrt, tan, atan, exp, log
+from math import radians, pi, cos, sin, atan, exp, dist
 from scipy.cluster.hierarchy import linkage, fcluster
 
 '''
-------------------------------------------
-This file contains Python implementation of the MRF-based triangulation procedure introduced in
+This file is the counterpart of the Python implementation of the MRF-based triangulation procedure introduced in
 "Automatic Discovery and Geotagging of Objects from Street View Imagery"
 by V. A. Krylov, E. Kenny, R. Dahyot.
-https://arxiv.org/abs/1708.08417
+https://arxiv.org/abs/1708.08417 to remove all geospatial coordinate transformations aimed at verifying the accuracy 
+of the approach and its sensitivity to the input variations and hard coded parameters. 
 
 version 1.1
 Copyright (c) ADAPT centre, Trinity College Dublin, 2018
 
-------------------------------------------
-The module takes the ouput of object detection and depth estimation deployed on the original image set. Each line in 
-the input CSV file defines a detected object with FOUR floating point values: camera positions (GPS latitude and 
-longitude), bearing from north clockwise in degrees towards the object in the panoramic image and the depth estimate. 
-The latter may be omitted or set to zero.
-
 The module performs triangulation, MRF optimization to establish the optimal object configuration and clustering.
-
-The output CSV contains the list of GPS-coordinates (latitude and longitude) of identified objects of interests and a 
-score value for each of these. The score is the number of individual views contributing to an object (greater or equal 
-to 2).
-------------------------------------------
 '''
 
 ###########################################
@@ -39,8 +28,6 @@ to 2).
 MaxObjectDstFromCam = 25  # Max distance from camera to objects (in meters)
 MaxDstInCluster = 1  # Maximal size of clusters employed (in meters)
 
-scaling_factor = 640.0/256
-
 # MRF optimization parameters
 ICMiterations = 15		# Number of iterations for ICM
 DepthWeight = 0.2		# weight alpha in Eq.(4)
@@ -48,16 +35,6 @@ ObjectMultiView = 0.2		# weight beta in  Eq.(4)
 StandAlonePrice = max(1 - DepthWeight - ObjectMultiView, 0)  # weight (1-alpha-beta) in Eq. (4)
 
 ###########################################
-
-
-# conversion from (lat,lon) to meters
-def LatLonToMeters(lat, lon):
-    # Converts given lat/lon in WGS84 Datum to XY in Spherical Mercator EPSG:4326
-    originShift = 2 * pi * 6378137 / 2.0
-    mx = lon * originShift / 180.0
-    my = log(tan((90+lat) * pi/360.0))/(pi/180.0)
-    my = my * originShift / 180.0
-    return mx, my
 
 
 # conversion from meters to (lat,lon)
@@ -70,24 +47,7 @@ def MetersToLatLon(mx, my):
     return lat, lon
 
 
-# haversine distance formula between two points specified by their GPS coordinates
-def haversine(lon1, lat1, lon2, lat2):
-    """
-    Calculate the great circle distance between two points 
-    on the earth (specified in decimal degrees)
-    """
-    # convert decimal degrees to radians 
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-    # haversine formula 
-    dlon = lon2 - lon1 
-    dlat = lat2 - lat1 
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a)) 
-    m = 6367000. * c
-    return m
-
-
-# calculating the intersection point between two rays (specified each by camera position and depth-estimated object
+# calculating the intersection  point between two rays (specified each by camera position and depth-estimated object
 # location). Refer to https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection where u = y, t = x
 def Intersect(Object1, Object2):
     latC1 = Object1[5]
@@ -99,6 +59,7 @@ def Intersect(Object1, Object2):
     latP2 = Object2[0]
     lonP1 = Object1[1]
     lonP2 = Object2[1]
+    print(latC1, lonC1, latP1, lonP1, latC2, lonC2, latP2, lonP2, flush=True)
     a1 = latP1 - latC1
     b1 = latP2 - latC2
     c1 = latC2 - latC1
@@ -106,7 +67,8 @@ def Intersect(Object1, Object2):
     a2 = lonP1 - lonC1
     b2 = lonP2 - lonC2
     c2 = lonC2 - lonC1
-    d = a2 * b1 - b2 * a1
+
+    d = a2*b1-b2*a1
     if d:
         y = (a1*c2 - a2*c1) / d
     else:
@@ -115,6 +77,7 @@ def Intersect(Object1, Object2):
         x = (b1*y+c1) / a1
     else:
         x = (b2*y+c2) / a2
+
     if (x < 0) or (y < 0):
         return -2, -2, 0, 0
     if (x > MaxObjectDstFromCam) or (y > MaxObjectDstFromCam):
@@ -183,8 +146,7 @@ def main(inputfilename, outputfilename):
         return
 
     if os.path.isfile(outputfilename):
-        print('A file with the specified ouput name already exists. Aborting.')
-        return
+        os.remove(outputfilename)
 
     try:
         f1 = open(outputfilename, 'w')
@@ -203,22 +165,19 @@ def main(inputfilename, outputfilename):
             if len(nums) < 3:
                 print('Broken entry ignored')
             if len(nums) < 4:  # if a depth estimate is not available
-                lat, lon, bearing, depth = float(nums[0]), float(nums[1]), float(nums[2]), 5
+                mx, my, bearing, depth = float(nums[0]), float(nums[1]), float(nums[2]), 5
             else:
-                lat, lon, bearing, depth = float(nums[0]), float(nums[1]), float(nums[2]), float(nums[3])
+                mx, my, bearing, depth = float(nums[0]), float(nums[1]), float(nums[2]), float(nums[3])
             if depth <= 0:
                 depth = 5
 
             # calculating the object positions from camera position + bearing + depth_estimate
-            mx, my = LatLonToMeters(lat, lon)
             br1 = radians(bearing)
-            yCP = my + depth * cos(br1) * scaling_factor  # depth-based positions
-            xCP = mx + depth * sin(br1) * scaling_factor
-            latp, lonp = MetersToLatLon(xCP, yCP)
-            yCP = my + 1.0 * cos(br1) * scaling_factor	 # normalized positions (at 1m distance from camera)
-            xCP = mx + 1.0 * sin(br1) * scaling_factor
-            latp1, lonp1 = MetersToLatLon(xCP, yCP)
-            ObjectsBase.append((latp1, lonp1, bearing, depth, 0, lat, lon, latp, lonp))
+            yCP = my + depth * cos(br1)  # depth-based positions
+            xCP = mx + depth * sin(br1)
+            yCP1 = my + 1.0 * cos(br1)	 # normalized positions (at 1m distance from camera)
+            xCP1 = mx + 1.0 * sin(br1)
+            ObjectsBase.append((xCP1, yCP1, bearing, depth, 0, mx, my, xCP, yCP))
 
     print("All detected objects: {0:d}".format(len(ObjectsBase)))
 
@@ -237,7 +196,11 @@ def main(inputfilename, outputfilename):
             print('Parsed {} object entries ({:.2f}%)'.format(i, 100. * i / len(ObjectsBase)))
         ObjectsDst[i, i] = -5
         for j in range(i+1, len(ObjectsBase)):
-            CamDstMtrs = haversine((ObjectsBase[i])[6], (ObjectsBase[i])[5], (ObjectsBase[j])[6], (ObjectsBase[j])[5])
+            c1_y = ObjectsBase[i][6]
+            c1_x = ObjectsBase[i][5]
+            c2_y = ObjectsBase[j][6]
+            c2_x = ObjectsBase[j][5]
+            CamDstMtrs = dist([c1_x, c1_y], [c2_x, c2_y])
 
             # cam_positions - same (less than 1m apart) or too far
             if CamDstMtrs < 0.5 or CamDstMtrs > MaxCamDst:
@@ -252,11 +215,13 @@ def main(inputfilename, outputfilename):
                 NumIntersects += 1
 
     print("All admissible intersections: {0:d}".format(NumIntersects))
+    print(f"Intersects: {Intersects}", flush=True)
 
     ObjectsConnectivity = np.zeros((len(ObjectsBase), len(ObjectsBase)), dtype=np.uint8)
     ObjectsConnectivityViableOptions = np.zeros(len(ObjectsBase), dtype=np.uint8)
     for i in range(len(ObjectsBase)):
         ObjectsConnectivityViableOptions[i] = np.count_nonzero(ObjectsDst[i, :] > 0)
+    print(ObjectsConnectivityViableOptions)
 
     #############################
     #           I C M           #
@@ -271,6 +236,7 @@ def main(inputfilename, outputfilename):
         # randomly select a viable pair of intersections testObject <-> testObjectPair
         testObject = np.random.randint(0, len(ObjectsBase))
         if ObjectsConnectivityViableOptions[testObject] == 0:  # no pairing possible (standalone - )
+            print(f'{testObject} is standalone with no pairings', flush=True)
             continue
 
         randnum = 1 + np.random.randint(0, ObjectsConnectivityViableOptions[testObject])
@@ -282,15 +248,18 @@ def main(inputfilename, outputfilename):
             if curcnt == randnum:
                 testObjectPair = i
                 break
+
         EnergyOld = CalcEnergyObject(ObjectsDst, ObjectsBase, ObjectsConnectivity, testObject)
         EnergyOld += CalcEnergyObject(ObjectsDst, ObjectsBase, ObjectsConnectivity, testObjectPair)
         ObjectsConnectivity[testObject, testObjectPair] = 1 - ObjectsConnectivity[testObject, testObjectPair]
         ObjectsConnectivity[testObjectPair, testObject] = 1 - ObjectsConnectivity[testObjectPair, testObject]
         EnergyNew = CalcEnergyObject(ObjectsDst, ObjectsBase, ObjectsConnectivity, testObject)
         EnergyNew += CalcEnergyObject(ObjectsDst, ObjectsBase, ObjectsConnectivity, testObjectPair)
+        print(f'EnergyOld: {EnergyOld}, EnergyNew: {EnergyNew}')
         if EnergyNew <= EnergyOld:
             # keep the connection between testObject and testObjectPair for this iteration
             chngcnt += 1
+            print(f'keep the connection {testObject}-{testObjectPair}, energy: {EnergyNew}', flush=True)
             continue
 
         # revert to the old configuration
@@ -299,18 +268,19 @@ def main(inputfilename, outputfilename):
     #############################
     #    C L U S T E R I N G    #
     #############################
-
-    mx, my = LatLonToMeters((ObjectsBase[0])[0], (ObjectsBase[0])[1])
+    mx, my = (ObjectsBase[0])[0], (ObjectsBase[0])[1]
     # cos(45) and sin(45) are all equal to 0.707, so d45 is the projected distance of MaxDisInCluster to x and y
-    d45 = 0.707 * MaxDstInCluster * scaling_factor
-    ax, ay = MetersToLatLon(mx + d45, my + d45)
-    ax1, ay1 = MetersToLatLon(mx, my)
+    d45 = 0.707 * MaxDstInCluster
+    ax, ay = mx + d45, my + d45
+    ax1, ay1 = mx, my
     MaxDegreeDstInCluster = ((ax - ax1) ** 2 + (ay - ay1) ** 2) ** 0.5
+    print(MaxDegreeDstInCluster)
 
     ICMintersect = []
     ifObjectIntersects = np.zeros(len(ObjectsBase), dtype=np.uint8)
     for i in range(len(ObjectsBase)):
         res = CalcAvrgObject(Intersects, ObjectsConnectivity, i)
+        print(f'{i}: {res}', flush=True)
         if res[0]:
             ifObjectIntersects[i] = 1
             ICMintersect.append((res[0], res[1]))
@@ -321,7 +291,7 @@ def main(inputfilename, outputfilename):
     print(IntersectClusters)
     NumClusters = IntersectClusters.shape[0]
     with open(outputfilename, "w") as inter:
-        inter.write("lat,lon,score\n")
+        inter.write("X,Y,score\n")
         for i in range(NumClusters):
             inter.write("{0:f},{1:f},{2:d}\n".format(IntersectClusters[i, 0]/IntersectClusters[i, 2],
                                                      IntersectClusters[i, 1]/IntersectClusters[i, 2],
