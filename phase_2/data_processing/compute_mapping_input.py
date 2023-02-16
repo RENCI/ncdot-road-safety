f6740829d338ed17e0c3727d8c34bd4bbc27db3d
hyi <hongyi@renci.org>
2023-02-16 09:30:46 -0500

    added more logic for removing disconnected line parts

--- phase_2/data_processing/compute_mapping_input.py
+++ phase_2/data_processing/compute_mapping_input.py
@@ -169,11 +169,19 @@ def compute_mapping_input(mapping_df, input_depth_image_path, mapped_image, path
                                     line_indices_x[lidx-interval][line_indices_x[lidx-interval] > last_idx] = 0
                                     recompute = True
 
-                        if left_interval == -1 and right_interval == -1:
-                            # remove disconnected smaller part from the left or right
+                        if left_interval == -1 or right_interval == -1:
                             split_indices, con_indices = consecutive(line_indices_x[lidx], step_size=2)
                             if any(split_indices) and len(split_indices) == 1:
-                                indices = con_indices[0] if len(con_indices[0]) < len(con_indices[1]) else con_indices[1]
+                                if left_interval > 0:
+                                    # remove right part of the disconnected part
+                                    indices = con_indices[1]
+                                elif right_interval > 0:
+                                    # remove left part of the disconnected part
+                                    indices = con_indices[0]
+                                else:
+                                    # remove disconnected smaller part from the left or right
+                                    indices = con_indices[0] if len(con_indices[0]) < len(con_indices[1]) \
+                                        else con_indices[1]
                                 for x in indices:
                                     labeled_data[line_indices_y[lidx][0], x] = 0
                 if recompute:
