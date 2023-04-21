321038887e04de8e03745512f464fa24eced83fa
hyi <hongyi@renci.org>
2023-04-21 15:44:36 -0400

    improved alignment

--- phase_2/data_processing/align_segmented_road_with_lidar.py
+++ phase_2/data_processing/align_segmented_road_with_lidar.py
@@ -6,6 +6,8 @@ import math
 from utils import get_camera_latlon_and_bearing_for_image_from_mapping, bearing_between_two_latlon_points, \
     load_pickle_data, IMAGE_HEIGHT
 
+FOCUS_LENGTH = 0.001
+CAMERA_LIDAR_Z_OFFSET = 5
 
 def compute_match(x, y, series_x, series_y):
     # compute match indices in (series_x, series_y) pairs based on which point in all points represented in
@@ -23,10 +25,11 @@ def transform_to_world_coordinate_system(input_df, cam_x, cam_y, cam_bearing, ca
     input_df.X = input_df.X - cam_x
     input_df.Y = input_df.Y - cam_y
     # Calculate the distance between the cam_x, cam_y point and the first two X, Y columns of input_3d_points
-    input_df['WORLD_Z'] = np.sqrt(input_df.X ** 2 + input_df.Y ** 2)
-    input_3d_gdf.WORLD_Z = input_3d_gdf.WORLD_Z * np.cos(input_df.BEARING)
-    input_df['WORLD_Y'] = input_df.Z * np.cos(cam_bearing-math.pi/2)
-    input_df['WORLD_X'] = input_df.X * np.cos(math.pi-cam_bearing) - input_df.Y * np.sin(math.pi-cam_bearing)
+    input_df['CAM_DIST'] = np.sqrt(input_df.X ** 2 + input_df.Y ** 2)
+    input_df['WORLD_Z'] = input_df.CAM_DIST * np.cos(input_df.BEARING)
+    input_df['WORLD_Y'] = (input_df.Z - cam_z) * np.cos(math.pi - cam_bearing)
+    # input_df['WORLD_X'] = input_df.CAM_DIST * np.sin(input_df.BEARING)
+    input_df['WORLD_X'] = input_df.X * np.cos(math.pi - cam_bearing) - input_df.Y * np.sin(math.pi - cam_bearing) - 6
     return input_df
 
 
@@ -104,17 +107,22 @@ if __name__ == '__main__':
     input_3d_gdf['BEARING'] = input_3d_gdf['BEARING'] - cam_br
 
     # get the lidar road vertex with closest distance to the camera location
-    nearest_idx = compute_match(proj_cam_x, proj_cam_y, input_3d_gdf['X'], input_3d_gdf['Y']),
-    cam_lidar_z = input_3d_gdf.iloc[nearest_idx].Z
+    nearest_idx = compute_match(proj_cam_x, proj_cam_y, input_3d_gdf['X'], input_3d_gdf['Y'])
+
+    # approximate lidar z for camera location
+    if input_3d_gdf.iloc[nearest_idx+1].Z > input_3d_gdf.iloc[nearest_idx].Z:
+        cam_lidar_z = input_3d_gdf.iloc[nearest_idx].Z - CAMERA_LIDAR_Z_OFFSET
+    else:
+        cam_lidar_z = input_3d_gdf.iloc[nearest_idx].Z + CAMERA_LIDAR_Z_OFFSET
     print(f'camera Z: {cam_lidar_z}')
 
     input_3d_gdf = transform_to_world_coordinate_system(input_3d_gdf, proj_cam_x, proj_cam_y, cam_br, cam_lidar_z)
 
     input_3d_gdf['PROJ_X'] = input_3d_gdf.apply(
-        lambda row: -row['WORLD_X'] / row['WORLD_Z'],
+        lambda row: FOCUS_LENGTH * row['WORLD_X'] / (FOCUS_LENGTH - row['WORLD_Z']),
         axis=1)
     input_3d_gdf['PROJ_Y'] = input_3d_gdf.apply(
-        lambda row: -row['WORLD_Y'] / row['WORLD_Z'],
+        lambda row: FOCUS_LENGTH * row['WORLD_Y'] / (FOCUS_LENGTH - row['WORLD_Z']),
         axis=1)
     # translate lidar road vertices to be centered at the origin along the x-axis
     min_proj_x = min(input_3d_gdf['PROJ_X'])
@@ -133,9 +141,9 @@ if __name__ == '__main__':
     scale_x = range_x / (max(input_3d_gdf['PROJ_X']) - min_proj_x)
     scale_y = range_y / (max(input_3d_gdf['PROJ_Y']) - min_proj_y)
     input_3d_gdf['PROJ_SCREEN_X'] = input_3d_gdf['PROJ_X'].apply(
-        lambda x: int((x - min_proj_x) * scale_x + 0.5))
+        lambda x: int((x - min_proj_x) * scale_x))
     input_3d_gdf['PROJ_SCREEN_Y'] = input_3d_gdf['PROJ_Y'].apply(
-        lambda y: int(IMAGE_HEIGHT - (y - min_proj_y) * scale_y + 0.5))
+        lambda y: int(IMAGE_HEIGHT - (y - min_proj_y) * scale_y))
     input_2d_df['MATCH_3D_INDEX'] = input_2d_df.apply(lambda row: compute_match(row['X'], row['Y'],
                                                                                 input_3d_gdf['PROJ_SCREEN_X'],
                                                                                 input_3d_gdf['PROJ_SCREEN_Y']),
