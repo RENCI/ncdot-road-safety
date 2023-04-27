bfe035f74e9c98221d6ded2cc05d4652f0587628
hyi <hongyi@renci.org>
2023-04-26 22:39:06 -0400

    added bearing constraint in extract lidar 3d points script

--- phase_2/data_processing/extract_lidar_3d_points.py
+++ phase_2/data_processing/extract_lidar_3d_points.py
@@ -1,7 +1,8 @@
 import argparse
 import geopandas as gpd
 import pickle
-from utils import haversine
+from utils import haversine, bearing_between_two_latlon_points
+import math
 
 
 if __name__ == '__main__':
@@ -16,6 +17,9 @@ if __name__ == '__main__':
     parser.add_argument('--camera_loc', type=str,
                         default=[35.7137581, -82.7346679],
                         help='camera loc to extract lidar vertices within a set threshold distance from')
+    parser.add_argument('--next_camera_loc', type=str,
+                        default=[35.7136764,-82.7346359],
+                        help='next camera loc to define camera/driving bearing direction')
     parser.add_argument('--distance_threshold', type=str,
                         #default=385,
                         default=190,
@@ -25,6 +29,7 @@ if __name__ == '__main__':
     input_lidar_shp_with_path = args.input_lidar_shp_with_path
     output_file = args.output_file
     clat, clon = args.camera_loc
+    next_clat, next_clon = args.next_camera_loc
     distance_threshold = args.distance_threshold
 
     # read shape file
@@ -39,10 +44,13 @@ if __name__ == '__main__':
     # geom_df is added as a geometry_y column in lidar_df while the initial geometry column is renamed as geometry_x
     lidar_df = lidar_df.merge(geom_df, left_index=True, right_index=True)
     lidar_df['distance'] = lidar_df.apply(lambda row: haversine(clon, clat, row['geometry_y']), axis=1)
+    cam_bearing = bearing_between_two_latlon_points(clat, clon, next_clat, next_clon, is_degree=False)
+    lidar_df['bearing_diff'] = lidar_df.apply(lambda row: abs(cam_bearing - bearing_between_two_latlon_points(
+        clat, clon, row['geometry_y'].y, row['geometry_y'].x, is_degree=False)), axis=1)
     print(lidar_df.shape)
-    lidar_df = lidar_df[lidar_df['distance'] < distance_threshold]
+    lidar_df = lidar_df[(lidar_df['distance'] < distance_threshold) & (lidar_df['bearing_diff'] < math.pi / 3)]
     print(lidar_df.shape)
-    lidar_df = lidar_df.drop(columns = ['Id', 'ORIG_FID', 'geometry_y', 'geometry_x', 'distance'])
+    lidar_df = lidar_df.drop(columns = ['Id', 'ORIG_FID', 'geometry_y', 'geometry_x', 'distance', 'bearing_diff'])
     vertices = [lidar_df.to_numpy()]
     with open(output_file, 'wb') as f:
         pickle.dump(vertices, f)
