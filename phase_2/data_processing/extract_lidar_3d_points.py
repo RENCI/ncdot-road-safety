268fcb3a0ce3ad9f97e7a79e7c6e9cc6421b842f
hyi <hongyi@renci.org>
2023-04-28 10:39:25 -0400

    refactored extract lidar 3d poits script for running whole route

--- phase_2/data_processing/extract_lidar_3d_points.py
+++ phase_2/data_processing/extract_lidar_3d_points.py
@@ -5,6 +5,34 @@ from utils import haversine, bearing_between_two_latlon_points
 import math
 
 
+def get_lidar_data_from_shp(lidar_shp_file_path):
+    # read shape file
+    df = gpd.read_file(lidar_shp_file_path)
+    # lidar_df should have columns Id, ORIG_FID, POINT_X, POINT_Y, POINT_Z, and geometry where geometry is
+    # a point geometry, e.g., POINT Z (891488.760 734099.780 2018.620)
+    # print(lidar_df.head())
+    # total_bounds show minx, miny, maxx, maxy bounds of all geometry points
+    # print(lidar_df.total_bounds)
+    # convert lidar_df projection to WGS84 lat/lon CRS
+    geom_df = df.geometry.to_crs(epsg=4326)
+    # geom_df is added as a geometry_y column in lidar_df while the initial geometry column is renamed as geometry_x
+    return df.merge(geom_df, left_index=True, right_index=True)
+
+
+def extract_lidar_3d_points_for_camera(df, cam_loc, next_cam_loc, dist_th=190):
+    clat, clon = cam_loc
+    next_clat, next_clon = next_cam_loc
+    df['distance'] = df.apply(lambda row: haversine(clon, clat, row['geometry_y']), axis=1)
+    cam_bearing = bearing_between_two_latlon_points(clat, clon, next_clat, next_clon, is_degree=False)
+    df['bearing_diff'] = df.apply(lambda row: abs(cam_bearing - bearing_between_two_latlon_points(
+        clat, clon, row['geometry_y'].y, row['geometry_y'].x, is_degree=False)), axis=1)
+    print(df.shape)
+    df = df[(df['distance'] < dist_th) & (df['bearing_diff'] < math.pi / 3)]
+    print(df.shape)
+    df = df.drop(columns=['Id', 'ORIG_FID', 'geometry_y', 'geometry_x', 'distance', 'bearing_diff'])
+    return [df.to_numpy()]
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process arguments.')
     parser.add_argument('--input_lidar_shp_with_path', type=str,
@@ -28,29 +56,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
     input_lidar_shp_with_path = args.input_lidar_shp_with_path
     output_file = args.output_file
-    clat, clon = args.camera_loc
-    next_clat, next_clon = args.next_camera_loc
+    camera_loc = args.camera_loc
+    next_camera_loc = args.next_camera_loc
     distance_threshold = args.distance_threshold
+    lidar_df = get_lidar_data_from_shp(input_lidar_shp_with_path)
+    vertices = extract_lidar_3d_points_for_camera(lidar_df, camera_loc, next_camera_loc)
 
-    # read shape file
-    lidar_df = gpd.read_file(input_lidar_shp_with_path)
-    # lidar_df should have columns Id, ORIG_FID, POINT_X, POINT_Y, POINT_Z, and geometry where geometry is
-    # a point geometry, e.g., POINT Z (891488.760 734099.780 2018.620)
-    # print(lidar_df.head())
-    # total_bounds show minx, miny, maxx, maxy bounds of all geometry points
-    # print(lidar_df.total_bounds)
-    # convert lidar_df projection to WGS84 lat/lon CRS
-    geom_df = lidar_df.geometry.to_crs(epsg=4326)
-    # geom_df is added as a geometry_y column in lidar_df while the initial geometry column is renamed as geometry_x
-    lidar_df = lidar_df.merge(geom_df, left_index=True, right_index=True)
-    lidar_df['distance'] = lidar_df.apply(lambda row: haversine(clon, clat, row['geometry_y']), axis=1)
-    cam_bearing = bearing_between_two_latlon_points(clat, clon, next_clat, next_clon, is_degree=False)
-    lidar_df['bearing_diff'] = lidar_df.apply(lambda row: abs(cam_bearing - bearing_between_two_latlon_points(
-        clat, clon, row['geometry_y'].y, row['geometry_y'].x, is_degree=False)), axis=1)
-    print(lidar_df.shape)
-    lidar_df = lidar_df[(lidar_df['distance'] < distance_threshold) & (lidar_df['bearing_diff'] < math.pi / 3)]
-    print(lidar_df.shape)
-    lidar_df = lidar_df.drop(columns = ['Id', 'ORIG_FID', 'geometry_y', 'geometry_x', 'distance', 'bearing_diff'])
-    vertices = [lidar_df.to_numpy()]
     with open(output_file, 'wb') as f:
         pickle.dump(vertices, f)
