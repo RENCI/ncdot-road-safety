cc8394c9e93e9c456d419e5b9afc734aeaa28ddf
hyi <hongyi@renci.org>
2023-05-09 11:36:09 -0400

    plot image boundary for testing

---
+++ phase_2/data_processing/plot_image_boundary.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+from utils import get_data_from_image
+from get_road_boundary_points import get_image_road_boundary_points
+
+
+image_file_name = 'data/d13_route_40001001011/oneformer/926005420241.png'
+image_width, image_height, seg_img = get_data_from_image(image_file_name)
+_, _, road_contours = get_image_road_boundary_points(image_file_name)
+print(f'number of returned contours: {len(road_contours)}')
+# Plot the boundary on top of the original image
+plt.imshow(seg_img)
+plt.plot(road_contours[0][:, 0], road_contours[0][:, 1], 'r-', linewidth=2)
+
+plt.show()
