b35a6f8362147dd82eb7662df3fe1a122481ba2e
hyi <hongyi@renci.org>
2023-06-01 14:50:16 -0400

    added jupyter notebook result with increased focal length

--- phase_2/data_processing/create_overlay_plot.py
+++ phase_2/data_processing/create_overlay_plot.py
@@ -1,30 +1,48 @@
 import argparse
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import pandas as pd
-from utils import load_pickle_data, IMAGE_HEIGHT
+from utils import load_pickle_data, IMAGE_HEIGHT, IMAGE_WIDTH
 
 
 parser = argparse.ArgumentParser(description='Process arguments.')
 parser.add_argument('--input_2d', type=str,
-                    default='data/d13_route_40001001011/oneformer/output/route_batch/input_2d_92600555627.pkl',
+                    default='data/d13_route_40001001011/oneformer/output/route_batch/input_2d_92600542024.pkl',
                     help='2d vertices')
 parser.add_argument('--input_3d_proj', type=str,
-                    default='data/d13_route_40001001011/oneformer/output/route_batch/lidar_project_info_926005556271.csv',
+                    default='data/d13_route_40001001011/oneformer/output/route_batch/lidar_project_info_926005420241.csv',
                     help='3d projection vertices')
+parser.add_argument('--overlay_bg_image_path', type=str,
+                    default='data/d13_route_40001001011/other/926005420241.jpg',
+                    help='original background image for overlay with the scatter plots')
+
+parser.add_argument('--show_bg_img', action="store_true",
+                    help='show the background image')
+
 
 args = parser.parse_args()
 input_2d = args.input_2d
 input_3d_proj = args.input_3d_proj
+overlay_bg_image_path = args.overlay_bg_image_path
+show_bg_img = args.show_bg_img
 
 input_2d_points = load_pickle_data(input_2d)
 
 input_3d_proj_df = pd.read_csv(input_3d_proj, usecols=['PROJ_SCREEN_X', 'PROJ_SCREEN_Y'], dtype=int)
+
+if show_bg_img:
+    bg_img = mpimg.imread(overlay_bg_image_path)
+
 plt.scatter(input_2d_points[:, 0], IMAGE_HEIGHT - input_2d_points[:, 1], s=20)
 plt.scatter(input_3d_proj_df['PROJ_SCREEN_X'], IMAGE_HEIGHT - input_3d_proj_df['PROJ_SCREEN_Y'], s=20)
-
+if show_bg_img:
+    plt.imshow(bg_img, extent=[0, IMAGE_WIDTH-1, 0, IMAGE_HEIGHT-1])
 plt.title('2D road vertices & 3D projected road vertices in screen coordinate system')
 plt.ylabel('Y')
 plt.xlabel('X')
-plt.grid(True)
+if show_bg_img:
+    plt.grid(False)
+else:
+    plt.grid(True)
 # plt.legend(loc='lower left')
 plt.show()
