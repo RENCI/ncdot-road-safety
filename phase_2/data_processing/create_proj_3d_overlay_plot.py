e3b10d254d78e0ca445ab08e6a5eb8586f339fce
hyi <hongyi@renci.org>
2023-07-24 17:03:32 -0400

    added another option to a script

--- phase_2/data_processing/create_proj_3d_overlay_plot.py
+++ phase_2/data_processing/create_proj_3d_overlay_plot.py
@@ -36,6 +36,8 @@ if __name__ == '__main__':
                                 'lidar_project_info_926005420241_depth.csv',
                         help='output depth data file name if depth data needs to be written out; otherwise, '
                              'leave it blank')
+    parser.add_argument('--show_lidar_proj', action="store_true",
+                        help='show lidar projection overlay or segmented road boundary pixel overlay')
     parser.add_argument('--show_3d_plot', action="store_false",
                         help='show 3D X-Y-Z plot or show 2D plot')
 
@@ -46,27 +48,33 @@ if __name__ == '__main__':
     input_2d = args.input_2d
     input_depth_image_filename_pattern = args.input_depth_image_filename_pattern
     output_depth_data_file = args.output_depth_data_file
+    show_lidar_proj = args.show_lidar_proj
     show_3d_plot = args.show_3d_plot
 
     df_2d = pd.read_csv(input_2d)
+    df_2d = df_2d[df_2d.Z > 2]
     df_3d = pd.read_csv(input_3d)
+    if show_lidar_proj:
+        loader = PFMLoader((IMAGE_WIDTH, IMAGE_HEIGHT), color=False, compress=False)
+        pfm = get_depth_data(loader, input_depth_image_filename_pattern.format(image_base_name=image_base_name))
+        df_3d = create_depth(pfm, df_3d, 'PROJ_SCREEN_X', 'PROJ_SCREEN_Y', dsf=depth_scaling_factor)
+        df_3d = transform_2d_points_to_3d(df_3d, INIT_CAMERA_PARAMS[FOCAL_LENGTH_X], INIT_CAMERA_PARAMS[FOCAL_LENGTH_Y],
+                                          IMAGE_WIDTH, IMAGE_HEIGHT, x_header='PROJ_SCREEN_X', y_header='PROJ_SCREEN_Y',
+                                          z_header='Z')
 
-    loader = PFMLoader((IMAGE_WIDTH, IMAGE_HEIGHT), color=False, compress=False)
-    pfm = get_depth_data(loader, input_depth_image_filename_pattern.format(image_base_name=image_base_name))
-    df_3d = create_depth(pfm, df_3d, 'PROJ_SCREEN_X', 'PROJ_SCREEN_Y', dsf=depth_scaling_factor)
-    df_3d = transform_2d_points_to_3d(df_3d, INIT_CAMERA_PARAMS[FOCAL_LENGTH_X], INIT_CAMERA_PARAMS[FOCAL_LENGTH_Y],
-                                      IMAGE_WIDTH, IMAGE_HEIGHT, x_header='PROJ_SCREEN_X', y_header='PROJ_SCREEN_Y',
-                                      z_header='Z')
-
-    if output_depth_data_file:
-        df_3d.to_csv(output_depth_data_file, index=False)
+        if output_depth_data_file:
+            df_3d.to_csv(output_depth_data_file, index=False)
 
     fig = plt.figure(figsize=(10, 8))
     if show_3d_plot:
         ax = fig.add_subplot(111, projection='3d')
-        ax.scatter3D(df_3d['WORLD_X'], df_3d['WORLD_Y'], df_3d['WORLD_Z'])
-        ax.scatter3D(df_3d['X_3D'], df_3d['Y_3D'], df_3d['Z'])
-        # ax.scatter3D(df_3d['WORLD_X'], df_3d['WORLD_Y'], df_3d['Z'])
+        if show_lidar_proj:
+            ax.scatter3D(df_3d['WORLD_X'], df_3d['WORLD_Y'], df_3d['WORLD_Z'])
+            ax.scatter3D(df_3d['X_3D'], df_3d['Y_3D'], df_3d['Z'])
+            # ax.scatter3D(df_3d['WORLD_X'], df_3d['WORLD_Y'], df_3d['Z'])
+        else:
+            ax.scatter3D(df_3d['WORLD_X'], df_3d['WORLD_Y'], df_3d['WORLD_Z'])
+            ax.scatter3D(df_2d['X_3D'], df_2d['Y_3D'], df_2d['Z'])
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
