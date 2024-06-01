fc31f600cf7caae6a1acaef487277758ab908a2d
hyi <hongyi@renci.org>
2024-06-01 18:29:21 -0400

    updated get manual registration image list to output updated input mapping file as well

--- phase_2/data_processing/get_manual_registration_image_list.py
+++ phase_2/data_processing/get_manual_registration_image_list.py
@@ -25,30 +25,31 @@ if __name__ == '__main__':
     map_df = pd.read_csv(input_sensor_mapping_file_with_path, usecols=['ROUTEID', 'MAPPED_IMAGE'], dtype=str)
     map_df.sort_values(by=['ROUTEID', 'MAPPED_IMAGE'], inplace=True, ignore_index=True)
 
-    input_df = pd.read_csv(input_file, usecols=['IMAGE_PATH'])
-    input_list = input_df['IMAGE_PATH'].tolist()
+    input_columns = ['IMAGE_PATH', 'SEGMENTATION_PATH']
+    input_df = pd.read_csv(input_file, usecols=input_columns)
+    input_img_paths = input_df['IMAGE_PATH'].tolist()
+    input_img_seg_paths = input_df['SEGMENTATION_PATH'].tolist()
     out_columns = ['ROUTE_ID', 'IMAGE_BASE_NAME', 'VFOV', 'POS_X', 'POS_Y', 'POS_Z', 'ROT_X', 'ROT_Y', 'ROT_Z']
     out_list = []
-    no_mapping_list = []
+    updated_input_list = []
     prev_image_width = prev_image_height = -1
-    for idx, img_path in enumerate(input_list):
-        image_width, image_height = get_image_resolution(img_path)
-        if image_width != prev_image_width or image_height != prev_image_height:
-            img_base_name = os.path.basename(img_path)[:-5]
-            mapped_image_df = map_df[map_df['MAPPED_IMAGE'] == img_base_name]
-            if len(mapped_image_df) > 0:
+    for idx, img_paths in enumerate(zip(input_img_paths, input_img_seg_paths)):
+        img_path, img_seg_path = img_paths
+        img_base_name = os.path.basename(img_path)[:-5]
+        mapped_image_df = map_df[map_df['MAPPED_IMAGE'] == img_base_name]
+        if len(mapped_image_df) > 0:
+            updated_input_list.append([img_path, img_seg_path])
+            image_width, image_height = get_image_resolution(img_path)
+            if image_width != prev_image_width or image_height != prev_image_height:
                 route_id = mapped_image_df.iloc[0].ROUTEID
                 out_list.append([route_id, img_base_name, '', '', '', '', '', '', ''])
                 prev_image_height = image_height
                 prev_image_width = image_width
                 print(f'{img_base_name} is added to list for manual registration with length {len(no_mapping_list)}')
-            elif img_base_name not in no_mapping_list:
-                no_mapping_list.append(img_base_name)
         if idx % 100000 == 0:
             print(f'{idx} images have been checked')
     out_df = pd.DataFrame(out_list, columns=out_columns)
     out_df.to_csv(output_file, index=False)
-    if len(no_mapping_list) > 0:
-        no_mapping_img_df = pd.DataFrame(no_mapping_list, columns=['Image with no metadata'])
-        no_mapping_img_df.to_csv(f'{os.path.splitext(output_file)[0]}_no_mapping_metadata.csv', index=False)
+    updated_input_df = pd.DataFrame(updated_input_list, columns=input_columns)
+    updated_input_df.to_csv(os.path.join(os.path.dirname(output_file), os.path.basename(input_file)), index=False)
     sys.exit(0)
