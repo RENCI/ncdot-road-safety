fca6c4fe5a5f92b0a175edfc572aa6460da1446a
hyi <hongyi@renci.org>
2024-09-08 16:40:41 -0400

    some update with plotting

--- phase_2/data_processing/plotting/create_alignment_overlay_plot.py
+++ phase_2/data_processing/plotting/create_alignment_overlay_plot.py
@@ -9,13 +9,13 @@ from data_processing.utils import load_pickle_data, LIDARClass
 
 parser = argparse.ArgumentParser(description='Process arguments.')
 parser.add_argument('--input_2d', type=str,
-                    default='../data/d13_route_40001001012/test/input_2d_88100042414.csv',
+                    default='../data/d13_route_40001001012/test/input_2d_88100042618.csv',
                     help='2d vertices')
 parser.add_argument('--input_3d_proj', type=str,
-                    default='../data/d13_route_40001001012/test/lidar_project_info_88100042414.csv',
+                    default='../data/d13_route_40001001012/test/lidar_project_info_88100042618.csv',
                     help='3d projection vertices')
 parser.add_argument('--overlay_bg_image_path', type=str,
-                    default='../data/d13_route_40001001012/images/881000424141.jpg',
+                    default='../data/d13_route_40001001012/images/881000426181.jpg',
                     help='original background image for overlay with the scatter plots')
 parser.add_argument('--use_lidar_proj_cols', type=list,
                     default=['PROJ_SCREEN_X', 'PROJ_SCREEN_Y', 'C', 'BOUND', 'SIDE'],
@@ -80,7 +80,8 @@ else:
     else:
         input_2d_df = pd.read_csv(input_2d)
         colors = np.where(input_2d_df['SIDE'] > 0, 'blue', 'green')
-        plt.scatter(input_2d_df['X'], image_height - input_2d_df['Y'], s=20, c=colors)
+        # plt.scatter(input_2d_df['X'], image_height - input_2d_df['Y'], s=20, c=colors)
+        plt.scatter(input_2d_df['X'], image_height - input_2d_df['Y'], s=20)
 
 if not show_lidar_road_only and 'BOUND' in use_lidar_proj_cols:
     bound_ldf = input_3d_proj_df[input_3d_proj_df['BOUND'] > 0]
@@ -98,7 +99,8 @@ if colormap:
     # plt.scatter(remain_ldf['PROJ_SCREEN_X'], image_height - remain_ldf['PROJ_SCREEN_Y'], s=10,
     #             c=remain_ldf['C'].map(colormap), label=remain_ldf['C'])
     colors_3d = np.where(remain_ldf['SIDE'] > 0, 'blue', 'green')
-    plt.scatter(remain_ldf['PROJ_SCREEN_X'], image_height - remain_ldf['PROJ_SCREEN_Y'], s=10, c=colors_3d)
+    # plt.scatter(remain_ldf['PROJ_SCREEN_X'], image_height - remain_ldf['PROJ_SCREEN_Y'], s=10, c=colors_3d)
+    plt.scatter(remain_ldf['PROJ_SCREEN_X'], image_height - remain_ldf['PROJ_SCREEN_Y'], s=10, c='blue')
 else:
     plt.scatter(remain_ldf['PROJ_SCREEN_X'], image_height - remain_ldf['PROJ_SCREEN_Y'], s=10)
 
